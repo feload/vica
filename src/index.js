5baff0be16206c4ab56804d2c792c97dbc711276
Felipe <feload@gmail.com>
2019-04-04 21:08:55 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+     <h1>Hello world13123!!</h1>
+  </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('root'));
